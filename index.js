3baa38183253083edcfac564dc035dad52423a28
Andrew Jordan <humandrift@gmail.com>
2017-10-17 22:38:44 -0400

    Added additional packages and tested on /

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express'),
-    //config = require('./server/configure'),
+    config = require('./server/configure'),
     app = express();
 
-	app.set('port', process.env.PORT || 3300);
-	//app.set('views', __dirname + '/views');
-	//app = config(app);
+app.set('port', process.env.PORT || 3300);
+app.set('views', __dirname + '/views');
+app = config(app);
 
-	app.get('/', function (req, res) {
-  		res.send('Hello World!')
-	})
+app.get('/', function (req, res) {
+	res.send('Hello, Andy!')
+})
 
 // Creating an HTTP server using our app object and tell it to listen
 // to connections
---
+++ server/configure.js
@@ -0,0 +1,33 @@
+var path = require('path'),
+    routes = require('./routes'),
+    hbs = require('express-handlebars');
+    express = require('express'),
+    router = express.Router(),
+    morgan = require('morgan'),
+    //methodOverride = require('method-override'),
+    errorHandler = require('errorhandler');
+
+module.exports = function (app) {
+    
+    //configuration 
+    //setting Handlebars as the rendering engine of choice
+    app.engine('hbs', hbs.create({
+        defaultLayout: 'main',
+        layoutsDir: app.get('views') + '/layouts',
+        partialsDir: [app.get('views') + '/partials'],
+    //     // helpers: {
+    //     //     timeago: function (timestamp) {
+    //     //         return moment(timestamp).startOf('minute').fromNow();
+    //     //     }
+    //     // }
+     }).engine);
+     app.set('view engine', 'hbs');
+
+    app.use('/public/', express.static(path.join(__dirname, '../public')));
+    app.use(morgan('dev'));
+    if ('development' === app.get('env')) {
+       app.use(errorHandler());
+    }
+
+    return app;
+};
