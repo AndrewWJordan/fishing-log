339364368fee49b3da557de79245beb874cf0755
Andrew Jordan <humandrift@gmail.com>
2017-10-20 22:40:15 -0400

    Fixed request URI error

--- models/logs.js
+++ models/logs.js
@@ -1,6 +1,6 @@
 // Pulling in logs from Cockpit CMS
 var request = require('request');
-request('localhost/cockpit-master/api/collections/get/logs?token=31e1edc3b2b4fcaf5a11173b2b8b88', function(error, response, body) {
+request('http://localhost/cockpit-master/api/collections/get/logs?token=31e1edc3b2b4fcaf5a11173b2b8b88', function(error, response, body) {
 	console.log(error);
 	console.log('response && response.statusCode');
 	console.log(body);
