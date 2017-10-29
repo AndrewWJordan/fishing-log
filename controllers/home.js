2c1cf917491efcb3161e3a836ab42dc2aa77c172
Andrew Jordan <humandrift@gmail.com>
2017-10-29 10:38:31 -0400

    Changed headless collection to logs

--- controllers/home.js
+++ controllers/home.js
@@ -2,19 +2,17 @@
 
 module.exports = {
 	index: function (req, res) {
-		var url = 'http://localhost/cockpit-master/api/collections/get/test?token=31e1edc3b2b4fcaf5a11173b2b8b88';
+		var url = 'http://localhost/cockpit-master/api/collections/get/logs?token=31e1edc3b2b4fcaf5a11173b2b8b88';
 
 		request(url, function (err, response, body) {
 			if(err) {
 				res.render('index', {story: null, error: 'Holy moly! Something went wrong!'});
 			} else {
 				var stories = JSON.parse(body);
-				console.log(stories);
 				if(stories == undefined) {
 					res.render('index', {story: null, error: "Man! We're striking out!"});
 				} else {
-					console.log(stories.entries);
-					res.render('index', {story: stories.entries, error: 'No errors!'});
+					res.render('index', {story: stories.entries, error: null});
 				}
 			}
 		})
